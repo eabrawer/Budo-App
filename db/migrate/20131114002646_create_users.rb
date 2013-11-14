97d8db8bd2de7d69a82b3612effe3bd0d6333f29
eabrawer <ericbrawer@gmail.com>
2013-11-14 01:52:40 -0500

    Creating user controller, views and form partial

--- db/migrate/20131114002646_create_users.rb
+++ db/migrate/20131114002646_create_users.rb
@@ -1,13 +1,13 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-	  t.string :email,            
-      t.string :name,             
-      t.string :province,
-      t.string :city,           
-      t.integer :age,            
-      t.text :description,        
-	  t.string :password_digest,
+	  t.string :email            
+      t.string :name             
+      t.string :province
+      t.string :city         
+      t.integer :age           
+      t.text :description    
+	    t.string :password_digest
       t.timestamps
     end
   end
