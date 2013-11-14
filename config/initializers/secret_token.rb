29b96b6e511975a9e262efcd50284d5036218145
eabrawer <ericbrawer@gmail.com>
2013-11-13 19:00:03 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Budo::Application.config.secret_key_base = '3c487137dccdc1909bda8998210f16d4de30f014c4caddc9645ae15f0fabf70a6da17bd9124895a80c115622810c4690c3930f1328a37725987de13f4f7c46bb'
